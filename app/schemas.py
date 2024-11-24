0455e85cf3c03dcf8da156d44d28752e33c7fb69
chzhxkd100 <chzhxkd100@naver.com>
2024-11-24 17:03:26 +0000

    initial commit

---
+++ app/schemas.py
@@ -0,0 +1,41 @@
+from typing import List, Union
+
+from pydantic import BaseModel
+
+
+class PasteBase(BaseModel):
+    title: str
+    content: str
+    pass
+
+
+class PasteCreate(PasteBase):
+    pass
+
+
+class Paste(PasteBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        from_attributes = True
+
+
+class UserBase(BaseModel):
+    username: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    pastes: List[Paste] = []
+
+    class Config:
+        from_attributes = True
+
+class UserDetail(User):
+    salt: str
+
